e4c3e537c963c6aa6526e7ee3a383fd84e7a6a50
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-03-15 17:50:32 +0600

    cors again

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
